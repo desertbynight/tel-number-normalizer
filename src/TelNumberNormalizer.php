99a55a49aa78101ed69a6e4aaafc78cbc41dbc3b
desertbynight <49094791+desertbynight@users.noreply.github.com>
2019-10-15 06:45:48 +0900

    Add files via upload

---
+++ src/TelNumberNormalizer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Iic;
+
+use \Iic\StringManipulate;
+
+class TelNumberNormalizer
+{
+    public $source;
+
+    public function __construct(string $source)
+    {
+        $this->source = $source;
+    }
+
+
+    public function filterItTo(string $outputFile)
+    {
+
+        // Get the file into an array.
+        $lines = file($this->source);
+
+        // Output Array init
+        $toCSVArray = [];
+
+        // Loop through our array
+        foreach ($lines as $line_num => $line) {
+            $line = StringManipulate::toHankaku($line);
+            $line = StringManipulate::invertPlusAndDoubleZero($line);
+            $line = StringManipulate::stripMinus($line);
+            $line = StringManipulate::sanitizeNumberInt($line);
+            $toCSVArray[] = $line;
+        }
+
+        $fp = fopen($outputFile, 'w');
+        fputcsv($fp, $toCSVArray);
+        fclose($fp);
+
+    }
+}
